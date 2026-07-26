17e6f8791a63b6f101d520c632163f8875357c44
Fyre Mael <fyremaelstrom@gmail.com>
2026-07-25 17:46:13 -0700

    Configure documentary mathematics rendering

---
+++ docs/javascripts/documentary-mathjax.js
@@ -0,0 +1,12 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['\\(', '\\)'], ['$', '$']],
+    displayMath: [['\\[', '\\]'], ['$$', '$$']],
+    processEscapes: true,
+    tags: 'none'
+  },
+  options: {
+    ignoreHtmlClass: 'no-mathjax',
+    processHtmlClass: 'arithmatex|gcl-monograph'
+  }
+};
