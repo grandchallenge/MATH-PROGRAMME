e5086dfc2164aeb6aa811af38fe82fe8bfba554d
Fyre Mael <fyremaelstrom@gmail.com>
2026-09-19 05:57:07 -0700

    CMDG CM4 P3-M: bind canonical CompHaus locally-constant adjunction

--- fixtures/formal/CMDG-NAT-CONCORDANCE-001/CMDGCondensedCM4P3GPointFunctional.lean
+++ fixtures/formal/CMDG-NAT-CONCORDANCE-001/CMDGCondensedCM4P3GPointFunctional.lean
@@ -283,7 +283,9 @@ theorem weightedFiniteBooleanMeasureHom_measureSolidification_evaluationWeight_a
       simp only [Functor.map_comp, NatTrans.comp_app, ConcreteCategory.comp_apply]
     have hlc :=
       CategoryTheory.Adjunction.homEquiv_leftAdjointUniq_hom_app
-        (CondensedSet.LocallyConstant.iso.{u}.homAdjunction)
+        (CompHausLike.LocallyConstant.adjunction
+          (P := fun _ : TopCat.{u} => True)
+          (hs := fun _ _ _ => ((CompHaus.effectiveEpi_tfae _).out 1 3).mp))
         (Condensed.discreteUnderlyingAdj (Type (u + 1)))
         (ULift.{u + 1, u} Q1.obj)
     have hlcPoint :=
